42cc701e6da9207fb9d7b57f49f962cb1cf38769
ysrael montes de oca <ysrael_1024gm@yahoo.com>
2014-01-06 11:47:35 -0400

    edit

--- wp-content/themes/eurolimoTheme/footer.php
+++ wp-content/themes/eurolimoTheme/footer.php
@@ -5,10 +5,31 @@
                 <h3>Our Car</h3>
             </div>
             <div class="col-md-3">
-                <h3>Testimonials</h3>
-                <div class="caption">
-                    Planning a wedding was extremely stressful, but the advice and support from Driven took away the stress of our journeys. Our Chauffeur was wonderful, extremely professional and nothing was too much trouble. He even took a detour to a picturesque spot which we mentioned on the day, very last minute. I’ll certainly be recommending them to all my friends. Thanks for making our day so special!
-                </div>
+                <h3>Testimonials</h3>                
+<!--<div class="caption"> Planning a wedding was extremely stressful, but the advice and support from Driven took away the stress of our journeys. Our Chauffeur was wonderful, extremely professional and nothing was too much trouble. He even took a detour to a picturesque spot which we mentioned on the day, very last minute. I’ll certainly be recommending them to all my friends. Thanks for making our day so special!</div>-->
+				<ul class="bxslider">
+				<?php
+							$type = 'testimonials';
+							$args=array(
+							  'post_type' => $type,
+							  'post_status' => 'publish',
+							  'orderby' => 'date',
+							  'order' => 'DESC',
+							  'posts_per_page'=>-1);								 
+							$myposts = new WP_Query( $args );
+                             if ( $myposts->have_posts() ) : 
+                                while ( $myposts->have_posts() ) : 
+                                    $myposts->the_post();    
+                            ?>
+                            <li>
+                                <div class="caption"><?php the_content();   ?></div>
+								<span class="autor"><?php the_title(); ?></span>
+                            </li>
+                            <?php endwhile;?>                            
+                            <?php endif; ?>
+                            <?php wp_reset_query(); ?>
+					<li><div class="caption"></div></li>
+				</ul>                
             </div>
             <div class="col-md-3">
                 <h3>Our Team</h3>
--- wp-content/themes/eurolimoTheme/js/plugins.js
+++ wp-content/themes/eurolimoTheme/js/plugins.js
@@ -23,6 +23,7 @@
     $('.bxslider').bxSlider({
       auto: true
     });
+	
     
 }());
 
