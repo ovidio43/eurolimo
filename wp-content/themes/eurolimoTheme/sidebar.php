ce1ab77cb3705b61d7238a4023523e1bd359f39c
ysrael montes de oca <ysrael_1024gm@yahoo.com>
2014-01-07 12:30:09 -0400

    edit

--- wp-content/themes/eurolimoTheme/sidebar.php
+++ wp-content/themes/eurolimoTheme/sidebar.php
@@ -12,7 +12,7 @@
                                 while ( $myposts->have_posts() ) : 
                                     $myposts->the_post();   
 									$featured = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'large',false);?>									                           
-                            <div style="position:relative;">
+                            <div class="item">
                                 <a href="<?php echo get_permalink( get_the_ID() );?>"><img src="<?php echo $featured['0'];?>" title="<?php the_title();?>"></a>
 								<h3 class="service-title"><?php the_title();?></h3>
 							</div>
