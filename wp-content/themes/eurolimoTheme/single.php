780deac6acf02c79499225fd5c639195b8e1ebf8
ysrael montes de oca <ysrael_1024gm@yahoo.com>
2014-01-07 16:43:33 -0400

    edit

--- wp-content/themes/eurolimoTheme/single.php
+++ wp-content/themes/eurolimoTheme/single.php
@@ -10,10 +10,32 @@
             <div class="left-content">
             <?php
                 // Start the Loop.
+				
+				
                 while ( have_posts() ) : the_post();
                     //get_template_part( 'content', 'page' );
-                the_content();
-                endwhile;
+					
+					$getid = get_post_type( get_the_ID());
+					
+					if(	$getid=="flet"){
+					
+							$images = get_field('image_gallery');
+							
+							if( $images ): 
+			?>
+							<div class="wrapper">
+								<ul class="bxslider">
+									<?php foreach( $images as $image ): ?>
+										<li>
+											<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />											
+										</li>
+									<?php endforeach; ?>
+								</ul>
+							</div>
+							<?php endif; ?>
+			<?php	} ?>					
+            <?php the_content();
+				  endwhile;
             ?>                
             </div>
             
