8d909da565aa90ead3b117998156d3be510dd988
aldoadachi <aldoadachi@altra.com.bo>
2014-02-17 10:37:38 -0400

    edit

--- wp-content/themes/eurolimoTheme/index.php
+++ wp-content/themes/eurolimoTheme/index.php
@@ -1,77 +1,81 @@
 <?php get_header(); ?>
 
-      <section id="main-slider">
-          <div class="wrapper">
-            <ul class="bxslider">              
-				<?php
-				$type = 'home-slider';
-				$args=array(
-				  'post_type' => $type,
-				  'post_status' => 'publish',
-				   'orderby' => 'rand',
-				  'order' => 'DESC',
-				  'posts_per_page'=>-1);								 
-				$myposts = new WP_Query( $args );
-				 if ( $myposts->have_posts() ) : 
-					while ( $myposts->have_posts() ) : 
-						$myposts->the_post();   
-						$featured = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'large',false);?>
-								<li>
-									<img src="<?php echo $featured['0'];?>" title="<?php the_title();?>" alt="<?php the_title();?>">
-								</li>
-                    <?php endwhile;?>                            
-				<?php endif; ?>
-                <?php wp_reset_query(); ?>
-            </ul>          
-          </div>
-      </section>
-      <section id="main-section">
-      <div class="wrap-main">
-          <div class="wrapper">
+<section id="main-slider">
+    <div class="wrapper">
+        <ul class="bxslider">              
+            <?php
+            $type = 'home-slider';
+            $args = array(
+                'post_type' => $type,
+                'post_status' => 'publish',
+                'orderby' => 'rand',
+                'order' => 'DESC',
+                'posts_per_page' => -1);
+            $myposts = new WP_Query($args);
+            if ($myposts->have_posts()) :
+                while ($myposts->have_posts()) :
+                    $myposts->the_post();
+                    $featured = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'large', false);
+                    ?>
+                    <li>
+                        <img src="<?php echo $featured['0']; ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>">
+                    </li>
+                <?php endwhile; ?>                            
+            <?php endif; ?>
+            <?php wp_reset_query(); ?>
+        </ul>          
+    </div>
+</section>
+<section id="main-section">
+    <div class="wrap-main">
+        <div class="wrapper">
             <div class="col-md-8">
                 <div class="col-md-6">
-                    <h3><?php the_field("second_title_home_text","option");?> </h3>
+                    <h3><?php the_field("second_title_home_text", "option"); ?> </h3>
                     <div class="caption">
-                     <?php the_field("second_block_home_text","option");?>               
+                        <?php the_field("second_block_home_text", "option"); ?>               
                     </div>
                 </div>            
                 <div class="col-md-6">
-                    <h3><?php the_field("first_title_home_text","option");?></h3>
+                    <h3><?php the_field("first_title_home_text", "option"); ?></h3>
                     <div class="caption">
-                    <?php the_field("first_block_home_text","option");?>                
+                        <?php the_field("first_block_home_text", "option"); ?>                
                     </div>
-                    <h3><?php the_field("thirth_title_home_text","option");?> </h3>
+                    <h3><?php the_field("thirth_title_home_text", "option"); ?> </h3>
                     <div class="caption">
-        <?php the_field("thirth_block_home_text","option");?>                 
+                        <?php the_field("thirth_block_home_text", "option"); ?>                 
                     </div>
                 </div>             
             </div>
             <div class="services col-md-4 no-padding"><h3>Services</h3> 
-        <?php
-              $type = 'fleet_services';
-              $args=array(
-                'post_type' => $type,
-                'post_status' => 'publish',
-                 'orderby' => 'rand',
-                'order' => 'DESC',
-                'posts_per_page'=>3);                
-              $myposts = new WP_Query( $args );
-                             if ( $myposts->have_posts() ) : 
-                                while ( $myposts->have_posts() ) : 
-                                    $myposts->the_post();   
-                  $featured = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'large',false);?>                                            
-                            <div class="col-md-12 service-img">
-                                <a href="<?php echo get_permalink( get_the_ID() );?>"><img src="<?php echo $featured['0'];?>" title="<?php the_title();?>"></a>
-                <h3 class="service-title"><?php the_title();?></h3>
-              </div>
-                            <?php endwhile;?>                            
-                            <?php endif; ?>
-                            <?php wp_reset_query(); ?>        
+                <?php
+                $type = 'fleet_services';
+                $args = array(
+                    'post_type' => $type,
+                    'post_status' => 'publish',
+                    'orderby' => 'rand',
+                    'order' => 'DESC',
+                    'posts_per_page' => 3);
+                $myposts = new WP_Query($args);
+                if ($myposts->have_posts()) :
+                    while ($myposts->have_posts()) :
+                        $myposts->the_post();
+                        $featured = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'large', false);
+                        ?>                                            
+                        <div class="col-md-12 service-img">
+                            <a href="<?php echo get_permalink(get_the_ID()); ?>"><img src="<?php echo $featured['0']; ?>" title="<?php the_title(); ?>"></a>
+                            <h3 class="service-title"><?php the_title(); ?></h3>
+                        </div>
+                    <?php endwhile; ?>                            
+                <?php endif; ?>
+                <?php wp_reset_query(); ?>        
             </div>            
-          </div>
-        <?php //echo $_SERVER['DOCUMENT_ROOT'];
-        //$_SERVER['DOCUMENT_ROOT'];?>
-			</div>
         </div>
-      </section>
+        <?php
+        //echo $_SERVER['DOCUMENT_ROOT'];
+//$_SERVER['DOCUMENT_ROOT'];
+        ?>
+    </div>
+</div>
+</section>
 <?php get_footer(); ?>
--- wp-content/themes/eurolimoTheme/template-pricing-form.php
+++ wp-content/themes/eurolimoTheme/template-pricing-form.php
@@ -1,128 +1,129 @@
-<?php 
+<?php
 /*
-* Template Name: pricing form
-*
-*/
-get_header(); ?>
+ * Template Name: pricing form
+ *
+ */
+get_header();
+?>
 <section id="main-title">
     <div class="wrapper">
-        <h1><?php the_title();?></h1>       
+        <h1><?php the_title(); ?></h1>       
     </div>
 </section>
 <section id="main-section">
     <div class="wrap-main">
         <div class="wrapper">
             <div class="full-content">
-            <?php
+                <?php
                 // Start the Loop.
-                while ( have_posts() ) : the_post();
+                while (have_posts()) : the_post();
                     //get_template_part( 'content', 'page' );
-                the_content();
+                    the_content();
                 endwhile;
-            ?>
-             <div class="wrap-form">
-                 <form>
-                     <h1>Choose Vehicle </h1>
-                     <div class="input_radio">
+                ?>
+                <div class="wrap-form">
+                    <form>
+                        <h1>Choose Vehicle </h1>
+                        <div class="input_radio">
                             <div class="input_radio_float">
-                               <span class="block_radio"></span>                         
-                               <input type="radio" name="choose-vehicle"> 
-                               <span class="imput_name">Landrover</span>
+                                <span class="block_radio"></span>                         
+                                <input type="radio" name="choose-vehicle"> 
+                                <span class="imput_name">Landrover</span>
                             </div>
                             <div class="input_radio_float">
                                 <span class="block_radio_one"> </span>                    
                                 <input type="radio" name="choose-vehicle"> 
                                 <span class="imput_name">Mercedez Benz GL550</span>
                             </div>
-                     </div>
-                     <h1>Trip Type</h1>
-                     <ul class="trip_type">
-                         <li><input type="radio" name="trip-type"> Single Destination</li>
-                         <li><input type="radio" name="trip-type"> Multiple Destinations</li>
-                         <li><input type="radio" name="trip-type"> Hourly / As Directed</li>
-                         <li><input type="radio" name="trip-type"> Multi-Day</li>
-                     </ul>
-                     <h1>Starting Address</h1>
-                     <ul class="block_imputs">
-                        <li>
-                             Pickup Address *<br>
-                             <input type="text" name="" class="imput_address">
-                        </li>
-                        <li>                         
-                             Pickup City * <br>
-                             <input type="text" name="" class="imput_address">
-                        </li>
-                        <li>
-                             Pickup State * <br>
-                             <select><option>-please select</option></select>
-                        </li>
-                      </ul>   
-                     <h1>Destination</h1>
-                     <ul class="block_imputs">
-                        <li>
-                             Destination Address * <br>
-                             <input type="text" name="" class="imput_address">
-                        </li>
-                        <li>                         
-                             Destination City *<br>
-                             <input type="text" name="" class="imput_address">
-                        </li>
-                        <li>
-                             Destination State *<br>
-                            <select><option>-please select</option></select>
-                        </li>
-                      </ul>   
-
-                     <h1>Contact Info</h1>
-                     <ul class="block_dates_imputs">
-                        <li>
-                             First Name *<br>
-                             <input type="text" name="">
-                        </li>
-                        <li>                         
-                             Last Name *<br>
-                             <input type="text" name="">
-                        </li>
-                        <li>
-                             Email *<br>
-                            <input type="email" name="">
-                        </li>
-                        <li>
-                            Phone number * <br>
-                            <input type="text" name="" class="cfone" >
-                        </li>
-                      </ul>   
-
-                     <h1>How did you hear about us?</h1>
-
-                     <ul class="block_dates_imputs">
-                        <li>
-                             If other, where: <br>
-                             <select><option>-please select</option></select>
-                        </li>
-                        <li>                         
-                            <br>
-                            <input type="text" name="">
-                        </li>
-                    </ul>
-                     <h1>Additional Comments</h1>
-                      <ul class="block_dates_end">
+                        </div>
+                        <h1>Trip Type</h1>
+                        <ul class="trip_type">
+                            <li><input type="radio" name="trip-type"> Single Destination</li>
+                            <li><input type="radio" name="trip-type"> Multiple Destinations</li>
+                            <li><input type="radio" name="trip-type"> Hourly / As Directed</li>
+                            <li><input type="radio" name="trip-type"> Multi-Day</li>
+                        </ul>
+                        <h1>Starting Address</h1>
+                        <ul class="block_imputs">
                             <li>
-                                 <textarea></textarea>
+                                Pickup Address *<br>
+                                <input type="text" name="" class="imput_address">
                             </li>
                             <li>                         
-                              <input type="checkbox" name="">  Send me news and special offers.  
+                                Pickup City * <br>
+                                <input type="text" name="" class="imput_address">
+                            </li>
+                            <li>
+                                Pickup State * <br>
+                                <select><option>-please select</option></select>
+                            </li>
+                        </ul>   
+                        <h1>Destination</h1>
+                        <ul class="block_imputs">
+                            <li>
+                                Destination Address * <br>
+                                <input type="text" name="" class="imput_address">
+                            </li>
+                            <li>                         
+                                Destination City *<br>
+                                <input type="text" name="" class="imput_address">
+                            </li>
+                            <li>
+                                Destination State *<br>
+                                <select><option>-please select</option></select>
+                            </li>
+                        </ul>   
+
+                        <h1>Contact Info</h1>
+                        <ul class="block_dates_imputs">
+                            <li>
+                                First Name *<br>
+                                <input type="text" name="">
+                            </li>
+                            <li>                         
+                                Last Name *<br>
+                                <input type="text" name="">
+                            </li>
+                            <li>
+                                Email *<br>
+                                <input type="email" name="">
+                            </li>
+                            <li>
+                                Phone number * <br>
+                                <input type="text" name="" class="cfone" >
+                            </li>
+                        </ul>   
+
+                        <h1>How did you hear about us?</h1>
+
+                        <ul class="block_dates_imputs">
+                            <li>
+                                If other, where: <br>
+                                <select><option>-please select</option></select>
+                            </li>
+                            <li>                         
+                                <br>
+                                <input type="text" name="">
+                            </li>
+                        </ul>
+                        <h1>Additional Comments</h1>
+                        <ul class="block_dates_end">
+                            <li>
+                                <textarea></textarea>
+                            </li>
+                            <li>                         
+                                <input type="checkbox" name="">  Send me news and special offers.  
                             </li>
                             <li>
                                 <input type="submit" name="" value="sumit request" class="submit">
                             </li>
-                    </ul>
-                 </form>
-             </div>   
+                        </ul>
+                    </form>
+                </div>   
             </div>
         </div>
     </div>
 </section>
 <?php
 get_footer();
- ?>
+?>
