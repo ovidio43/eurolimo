1aa22b3b38a15b6ed4c65dfb6bfe05624674b566
aldoadachi <aldoadachi@altra.com.bo>
2014-02-21 14:55:44 -0400

    edit

--- wp-content/themes/eurolimoTheme/include/pricing-form.php
+++ wp-content/themes/eurolimoTheme/include/pricing-form.php
@@ -4,16 +4,16 @@
             rules: {
                 'choose-vehicle': {required: true},
                 'trip-type': {required: true},
-                'aickup-address': {required: true},
-                'pickup-city': {required: true},
-                'pickup-state': {required: true},
+                'pickup-address': {required: true},
                 'destination-address': {required: true},
-                'destination-city': {required: true},
-                'destination-state': {required: true},
                 'first-name': {required: true},
                 'last-name': {required: true},
-                'email': {required: true,email:true},
-                'phone-number': {required: true,number:true}
+                'email': {required: true, email: true},
+                'phone-number': {required: true, number: true}
+            },
+            messages: {
+                'choose-vehicle': "Please select one option.",
+                'trip-type': "Please select one option."
             }
         });
     });
@@ -37,90 +37,66 @@
                 $featured = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'medium', false);
                 ?>                                            
 
-        <div class="input_radio_float">
-            <span class="block_radio" style="background-image:url(<?php echo $featured['0']; ?>);"></span>                         
-            <input type="radio" name="choose-vehicle" value="<?php the_field('thumbnail_headline_text'); ?>"> 
-            <span class="imput_name"><?php the_field('thumbnail_headline_text'); ?></span>
-        </div>                
+                <div class="input_radio_float">
+                    <label class="selected" for="radio<?php echo get_the_ID(); ?>">
+                        <span class="block_radio" style="background-image:url(<?php echo $featured['0']; ?>);"></span>
+                    </label>
+                    <input class="radio-selected" type="radio" id="radio<?php echo get_the_ID(); ?>" name="choose-vehicle" value="<?php the_field('thumbnail_headline_text'); ?>"> 
+                    <span class="imput_name"><?php the_field('thumbnail_headline_text'); ?></span>
+                </div>                
             <?php endwhile; ?>                            
         <?php endif; ?>
         <?php wp_reset_query(); ?>
 
     </div>
+    <script type="text/javascript">
+     
+    </script>
     <h1>Trip Type</h1>
     <ul class="trip_type">
-        <li><input type="radio" name="trip-type" value="Single Destination"> Single Destination</li>
-        <li><input type="radio" name="trip-type" value="Multiple Destinations"> Multiple Destinations</li>
-        <li><input type="radio" name="trip-type" value="Hourly / As Directed"> Hourly / As Directed</li>
-        <li><input type="radio" name="trip-type" value="Multi-Day"> Multi-Day</li>
+        <li><input type="radio" name="trip-type" value="Single Destination" id="op1"> <label for="op1"> Single Destination</label></li>
+        <li><input type="radio" name="trip-type" value="Multiple Destinations" id="op2"><label for="op2"> Multiple Destinations</label></li>
+        <li><input type="radio" name="trip-type" value="Hourly / As Directed" id="op3"><label for="op3"> Hourly / As Directed</label></li>
+        <li><input type="radio" name="trip-type" value="Multi-Day" id="op4"><label for="op4"> Multi-Day</label></li>
     </ul>
     <h1>Starting Address</h1>
     <ul class="block_imputs">
         <li>
-            Pickup Address *<br>
+            Pickup address <span class="required-ast">*</span><br>
             <input type="text" name="pickup-address" class="imput_address">
-        </li>
-        <li>                         
-            Pickup City * <br>
-            <input type="text" name="pickup-city" class="imput_address">
-        </li>
-        <li>
-            Pickup State * <br>
-            <select name="pickup-state">
-                <option>-please select</option>
-                <option value="Pickup State">Pickup State</option>
-            </select>
-        </li>
+        </li>         
     </ul>   
     <h1>Destination</h1>
     <ul class="block_imputs">
         <li>
-            Destination Address * <br>
+            Destination address <span class="required-ast">*</span> <br>
             <input type="text" name="destination-address" class="imput_address">
-        </li>
-        <li>                         
-            Destination City *<br>
-            <input type="text" name="destination-city" class="imput_address">
-        </li>
-        <li>
-            Destination State *<br>
-            <select name="destination-state">
-                <option>-please select</option>
-                <option value="Destination State">Destination State</option>
-            </select>
-        </li>
+        </li>      
     </ul>   
 
     <h1>Contact Info</h1>
     <ul class="block_dates_imputs">
         <li>
-            First Name *<br>
+            First Name <span class="required-ast">*</span><br>
             <input type="text" name="first-name">
         </li>
         <li>                         
-            Last Name *<br>
+            Last Name <span class="required-ast">*</span><br>
             <input type="text" name="last-name">
         </li>
         <li>
-            Email *<br>
+            Email <span class="required-ast">*</span><br>
             <input type="text" name="email">
         </li>
         <li>
-            Phone number * <br>
+            Phone number <span class="required-ast">*</span> <br>
             <input type="text" name="phone-number" class="cfone" >
         </li>
     </ul>   
 
     <h1>How did you hear about us?</h1>
 
-    <ul class="block_dates_imputs">
-        <li>
-            If other, where: <br>
-            <select name="if-other-where">
-                <option>-please select</option>
-                <option value="If other, where: ">If other, where: </option>
-            </select>
-        </li>
+    <ul class="block_dates_imputs">      
         <li>                         
             <br>
             <input type="text" name="if-other-where2">
@@ -132,12 +108,13 @@
             <textarea name="additional-comments"></textarea>
         </li>
         <li>                         
-            <input type="checkbox" name="Send-me-news">  Send me news and special offers.  
+            <input type="checkbox" name="Send-me-news" checked="">  Send me news and special offers.  
         </li>
         <li>
-            <input type="submit"  value="sumit" class="submit">
+            <input type="submit"  value="Sumit" class="submit">
             <input type="hidden" name="step" value="send">
-            <label class="notice">* Required</label>
-        </li>
+
+        </li>        
     </ul>
+    <label class="notice"><span class="required-ast">*</span> Required</label>
 </form>
--- wp-content/themes/eurolimoTheme/js/main.js
+++ wp-content/themes/eurolimoTheme/js/main.js
@@ -1,1 +1,10 @@
-
+jQuery(document).ready(function() {
+    jQuery('.selected').on('click', function() {
+        jQuery('.block_radio').removeClass('selected-on');
+        jQuery(this).children().addClass('selected-on');
+    });
+    jQuery('.radio-selected').on('click', function() {
+        jQuery('.block_radio').removeClass('selected-on');
+        jQuery(this).siblings('.selected').children().addClass('selected-on');
+    });
+});
